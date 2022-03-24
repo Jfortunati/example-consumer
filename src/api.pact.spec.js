fc33ddce16e826fc74a9f7f3e0ecbdf937970ea4
Jonathan.Fortunati@smartbear.com <jonathan.fortunati@smartbear.com>
2022-03-24 11:32:53 -0400

    Changed price to trigger a build

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -20,7 +20,7 @@ describe('API Pact test', () => {
       // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees'}
 
       // Uncomment to see this fail
-      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: '29.99'}
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: '29.00'}
       
       // Uncomment to see this fail
       // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
