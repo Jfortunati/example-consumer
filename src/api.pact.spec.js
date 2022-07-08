0cf05811b8e02034e24fe5ca5b0effe71eca1bbf
Jfortunati <Jonathan.Fortunati@smartbear.com>
2022-07-08 11:41:26 -0400

    Reset consumer test

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -17,10 +17,10 @@ describe('API Pact test', () => {
   describe('retrieving a product', () => {
     test('ID 10 exists', async () => {
       // Arrange
-      // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees'}
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees'}
 
       // Uncomment to see this fail
-      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: '29.00'}
+      // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: '29.00'}
       
       // Uncomment to see this fail
       // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees'}
